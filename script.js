40995bbd0490686aa81b9c73c32468455e42800a
Chris Hicks <chrish@birch.coop>
2020-03-08 18:28:52 -0700

    fix unicode

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var view = document.getElementsByClassName("view")[0];
 view.id = "view"
 view.innerHTML = ' \
   \
-  &bull <a href="https://www.facebook.com/hickscme" target="_blank">Facebook</a> \
-  &bull <a href="https://github.com/spudunk" target="blank">GitHub</a> \
+  &bull; <a href="https://www.facebook.com/hickscme" target="_blank">Facebook</a><br> \
+  &#8226; <a href="https://github.com/spudunk" target="blank">GitHub</a> \
   \
 ';
 view.style.visibility = "visible";
