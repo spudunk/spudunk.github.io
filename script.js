ac19c144a3f2ee4ecb6e65ebc2916c813725d486
Chris Hicks <chrish@birch.coop>
2020-03-08 18:14:37 -0700

    fix multiline string

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var view = document.getElementsByClassName("view")[0];
 view.id = "view"
-view.innerHTML = '
-  <a href="https://www.facebook.com/hickscme" target="_blank">Facebook</a><br>
-  <a href="https://github.com/spudunk" target="blank">GitHub</a>
+view.innerHTML = ' \
+  <a href="https://www.facebook.com/hickscme" target="_blank">Facebook</a><br> \
+  <a href="https://github.com/spudunk" target="blank">GitHub</a> \
 ';
 view.style.visibility = "visible";
