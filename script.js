8cf44e806e73cd6ce0bbcd0d30d8388e683d0a7d
Chris Hicks <chrish@birch.coop>
2020-03-08 18:49:42 -0700

    multiple updates

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 var view = document.getElementsByClassName("view")[0];
 // view.id = "view"
-view.innerHTML = ' \
-  &bull; <a href="https://www.facebook.com/hickscme" target="_blank">Facebook</a><br> \
-  &bull; <a href="https://github.com/spudunk" target="blank">GitHub</a> \
-';
+view.innerHTML = `
+  &bull; <a href="https://www.facebook.com/hickscme" target="_blank">Facebook</a><br>
+  &bull; <a href="https://github.com/spudunk" target="_blank">GitHub</a><br>
+  &bull; <a href="https://twitter.com/HicksCME" target="_blank">Twitter</a><br>
+`;
 view.style.visibility = "visible";
 contact = document.getElementById("contact");
 contact.innerHTML = `
-  <p>Contact me at: </p>
+  <p>Contact me at: <br>
   <a href="tel:+13608272736">(360) 827-2736</a><br>
-  <a href="mailto:hickscme@gmail.com">hickscme@gmail.com</a>
+  <a href="mailto:hickscme@gmail.com">hickscme@gmail.com</a></p>
 `
