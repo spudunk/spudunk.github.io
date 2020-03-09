7b68fdb20a9eb1d2aa2885de417323a35958d913
Chris Hicks <chrish@birch.coop>
2020-03-08 18:11:51 -0700

    bug fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var view = document.getElementsByClassName("view")[0];
-view.id = "view"''
+view.id = "view"
 view.innerHTML = '
   <a href="https://www.facebook.com/hickscme" target="_blank">Facebook</a><br>
   <a href="https://github.com/spudunk" target="blank">GitHub</a>
