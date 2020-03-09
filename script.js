cb0cb973708eecbf8681945a85a6b7ee328db626
Chris Hicks <chrish@birch.coop>
2020-03-08 18:25:08 -0700

    remove list, add bullets

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var view = document.getElementsByClassName("view")[0];
 view.id = "view"
 view.innerHTML = ' \
-  <ul> \
-    <li><a href="https://www.facebook.com/hickscme" target="_blank">Facebook</a></li> \
-    <li><a href="https://github.com/spudunk" target="blank">GitHub</a></li> \
-  </ul> \
+  \
+  &bull <a href="https://www.facebook.com/hickscme" target="_blank">Facebook</a> \
+  &bull <a href="https://github.com/spudunk" target="blank">GitHub</a> \
+  \
 ';
 view.style.visibility = "visible";
