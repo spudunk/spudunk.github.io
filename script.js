d83ed2d8ceff03f5632f1744ddf6833e90ce65b6
Chris Hicks <chrish@birch.coop>
2020-03-08 18:02:04 -0700

    make view element visible

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-var viewBlock = document.getElementsByClassName("view")[0];
-viewBlock.innerHTML = '<a href="https://www.facebook.com/hickscme" target="_blank">Facebook</a>';
+var view = document.getElementsByClassName("view")[0];
+view.id = "view"''
+view.innerHTML = '
+  <a href="https://www.facebook.com/hickscme" target="_blank">Facebook</a><br>
+  <a href="https://github.com/spudunk" target="blank">GitHub</a>
+';
+view.style.visibility = "visible";
