7fcf764000a780e458465ebfedcc3d0413b6ad63
Chris Hicks <chrish@birch.coop>
2020-06-25 22:46:52 -0700

    comment

--- asana-dashboard/script.js
+++ asana-dashboard/script.js
@@ -80,22 +80,25 @@
 
         setProjectId();
 
-        // Get User Info and add to user element
+        // Get User Info and add table to user element
         client.users.me()
             .then(function (me) {
                 console.log(me);
                 document.getElementById("user").innerHTML =
-                    `<table >
+                    `<table>
                     <tr><td colspan="2"><strong>${me.name}</strong></td></tr>
                     <tr><td>Email</td><td>${me.email}</td></tr>
                     <tr><td>Asana GID</td><td>${me.gid}</td></tr>
                 </table>`;
             });
-
+        
+        // Reset contentDiv with empty table
         contentDiv.innerHTML =
             `<table id="taskTable">
                 <tr><th>Name</th><th>Status</th><th>Flags</th></tr>
             </table>`;
+        
+        // re-assign table variable to new table created in DOM
         taskTable = document.getElementById("taskTable");
 
         // Get User Tasks
@@ -103,16 +106,18 @@
                 project: projectId,
                 opt_fields: "name,completed,assignee.(name|email),custom_fields.(enum_value|name)"
             })
+            // add each task to the table
             .then(function (tasks) {
-                console.log(tasks.data);
+                console.log(tasks.data);  // for debugging
                 tasks.data.forEach(task => {
                     addTask(task);
                 });
             });
     };
 
+    // Add task to table
     function addTask(task) {
-        // create a table row for the task
+        // create a table row element for the task
         tr = document.createElement("tr")
         // add task name cell to html string for insert to row
         contentString = `<td>${task.name}</td>`
@@ -124,8 +129,9 @@
             contentString +=
                 `<td style="color:red"><strong>INCOMPLETE</strong></td>`
         }
-
+        // create cell with list of Asana "custom fields" values
         customFieldString = "<td>";
+        // loop through custom fields and add enum_values
         task.custom_fields.forEach(field => {
             if (field.enum_value) {
                 // console.log(field);
@@ -134,9 +140,11 @@
 
             }
         });
+        // close custom fields cell
         customFieldString += '</td>';
+        // add custom field cell to table content string
         contentString += customFieldString;
-
+        // 
         tr.innerHTML = contentString;
         taskTable.appendChild(tr);
 
