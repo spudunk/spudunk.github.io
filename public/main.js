d07caf89d18b8c01f4b9487613cc4341f68d0179
spudunk <hicksme@gmail.com>
2022-07-04 09:34:57 -0700

    activate navbar

--- public/main.js
+++ public/main.js
@@ -3,24 +3,41 @@ var heroDown = document.getElementById("hero-down");
 var heroButton = document.getElementById("hero-button-primary");
 var about = document.getElementById("about");
 // var contact = document.getElementById("contact");
+var navbar = document.getElementById("navbar");
+var toggle = document.getElementById("menu-toggle");
 
-function fadeOutOnScroll(element) {
-  // FADE OUT HERO DOWN ARROW ON SCROLL DOWN
-  if (!element) {
-    return;
+navbar.style.display = "none";
+toggle.onclick = function (e) {
+  // console.log(e);
+  if (navbar.style.display == "none") {
+    navbar.style.display = "flex";
+  } else {
+    navbar.style.display = "none";
   }
-  var scrollTop = document.documentElement.scrollTop;
-  var opacity = 1;
-  if (scrollTop > 0) {
-    opacity = Math.max(1 - scrollTop / 100, 0);
-  }
-  element.style.opacity = opacity;
-}
+  return;
+};
+navbar.onclick = function (e) {
+  navbar.style.display = "none";
+  return;
+};
 
-function scrollHandler() {
-  // fadeOutOnScroll(heroDown);
-  // shrinkOnScroll(hero);
-}
+// function fadeOutOnScroll(element) {
+//   // FADE OUT HERO DOWN ARROW ON SCROLL DOWN
+//   if (!element) {
+//     return;
+//   }
+//   var scrollTop = document.documentElement.scrollTop;
+//   var opacity = 1;
+//   if (scrollTop > 0) {
+//     opacity = Math.max(1 - scrollTop / 100, 0);
+//   }
+//   element.style.opacity = opacity;
+// }
+
+// function scrollHandler() {
+// fadeOutOnScroll(heroDown);
+// shrinkOnScroll(hero);
+// }
 
 // window.addEventListener("scroll", scrollHandler);
 
